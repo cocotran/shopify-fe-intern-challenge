6de4ff5231f910f598bd6ea16363995e205ec64e
cocotran <anhquangtran2703@gmail.com>
2022-05-09 17:24:04 -0400

    Add basic HTML layout

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,1 +1,38 @@
-const hello: string = "Hello"
+// ============= Classes, interface =================
+interface Result {
+    prompt: string,
+    response: string
+}
+
+// ============= Constants, helpers =================
+const BASE_URL: string = "";
+
+const sendRequest = async (data: any) => {
+    const res = await fetch(BASE_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.SECRET}`
+        },
+        body: JSON.stringify(data)
+    });
+    return res.json();
+}
+
+// ============= Application state =================
+const results: Result[] = [];
+
+// ============= State functions =================
+
+// ============= DOM node references =================
+
+// ============= DOM update functions =================
+
+// ============= Event handlers =================
+const onSubmitBtn = async () => {
+
+}
+
+// ============= Event handler bindings =================
+
+// ============= Initial setup =================
