6cff5cbd193a609cff333981060dd5e1b83b076f
cocotran <anhquangtran2703@gmail.com>
2022-05-09 23:57:25 -0400

    Add DOM manipulation method

---
+++ src/scripts/index.js
@@ -0,0 +1,86 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+// ============= Constants, helpers =================
+var BASE_URL = "";
+function htmlToElement(htmlString) {
+    var template = document.createElement('template');
+    htmlString = htmlString.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = htmlString;
+    return template.content.firstChild;
+}
+var sendRequest = function (data) { return __awaiter(_this, void 0, void 0, function () {
+    var res;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, fetch(BASE_URL, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: "Bearer ".concat(process.env.SECRET)
+                    },
+                    body: JSON.stringify(data)
+                })];
+            case 1:
+                res = _a.sent();
+                return [2 /*return*/, res.json()];
+        }
+    });
+}); };
+// ============= Application state =================
+var results = [];
+// ============= State functions =================
+// ============= DOM node references =================
+var responseContainer = document.getElementById("responseContainer");
+var submitBtn = document.getElementById("submitBtn");
+// ============= DOM update functions =================
+var displayNewResult = function (prompt, response) {
+    var newResult = "\n        <div style=\"border: 1px solid blue\">\n            <h5>".concat(prompt, "</h5>\n            <p>\n                ").concat(response, "\n            </p>\n        </div>\n    ");
+    if (htmlToElement(newResult))
+        responseContainer === null || responseContainer === void 0 ? void 0 : responseContainer.appendChild(htmlToElement(newResult));
+};
+// ============= Event handlers =================
+var onSubmitBtn = function () { return __awaiter(_this, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        console.log("Here");
+        displayNewResult("test prompt", "testtttt response");
+        return [2 /*return*/];
+    });
+}); };
+// ============= Event handler bindings =================
+submitBtn === null || submitBtn === void 0 ? void 0 : submitBtn.addEventListener('click', onSubmitBtn);
+// ============= Initial setup =================
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,23 +1,30 @@
 // ============= Classes, interface =================
 interface Result {
-    prompt: string,
-    response: string
+  prompt: string;
+  response: string;
 }
 
 // ============= Constants, helpers =================
 const BASE_URL: string = "";
 
-const sendRequest = async (data: any) => {
-    const res = await fetch(BASE_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.SECRET}`
-        },
-        body: JSON.stringify(data)
-    });
-    return res.json();
+function htmlToElement(htmlString: string) {
+    var template = document.createElement('template');
+    htmlString = htmlString.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = htmlString;
+    return template.content.firstChild;
 }
+
+const sendRequest = async (data: any) => {
+  const res = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.SECRET}`,
+    },
+    body: JSON.stringify(data),
+  });
+  return res.json();
+};
 
 // ============= Application state =================
 const results: Result[] = [];
@@ -25,14 +32,30 @@ const results: Result[] = [];
 // ============= State functions =================
 
 // ============= DOM node references =================
+const responseContainer = document.getElementById("responseContainer");
+const submitBtn = document.getElementById("submitBtn");
 
 // ============= DOM update functions =================
+const displayNewResult = (prompt: string, response: string) => {
+    const newResult = `
+        <div style="border: 1px solid blue">
+            <h5>${prompt}</h5>
+            <p>
+                ${response}
+            </p>
+        </div>
+    `
+    if (htmlToElement(newResult))
+        responseContainer?.appendChild(htmlToElement(newResult) as ChildNode);
+}
 
 // ============= Event handlers =================
 const onSubmitBtn = async () => {
-
-}
+    console.log("Here");
+    displayNewResult("test prompt", "testtttt response");
+};
 
 // ============= Event handler bindings =================
+submitBtn?.addEventListener('click', onSubmitBtn);
 
 // ============= Initial setup =================
